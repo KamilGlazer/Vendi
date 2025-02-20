a2a2927f6b052a3d4d86416fd8d961ecacd8691d
Kamil Glazer <k.glazer.kamil@interia.pl>
2025-02-20 20:06:25 +0100

    Adding some tests

--- Vendi/src/main/java/com/kamilglazer/Vendi/dto/response/JwtResponse.java
+++ Vendi/src/main/java/com/kamilglazer/Vendi/dto/response/JwtResponse.java
@@ -1,11 +1,15 @@
 package com.kamilglazer.Vendi.dto.response;
 
 import com.kamilglazer.Vendi.domain.USER_ROLE;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class JwtResponse {
     private String jwt;
     private String message;
